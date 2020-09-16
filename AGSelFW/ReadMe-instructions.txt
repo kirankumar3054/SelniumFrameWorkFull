73b51ecf72876ca1c85ecf11214cd4d46f3e98c2
kirankumar3054 <kirankumar3054@gmail.com>
2020-09-16 18:01:54 -0400

    Third Party Configuration added. i.e. SauceLabs here

--- AGSelFW/ReadMe-instructions.txt
+++ AGSelFW/ReadMe-instructions.txt
@@ -1,9 +1,16 @@
 
-1st option - you can use config.Properties file for parameters
+This framework is cable of run scripts local system as well as SauceLabs.  
+And it is configured to take sauce user & access key from local terminal (which are already exported to local like below example:)
+export SAUCE_USERNAME=krudrangi SAUCE_ACCESS_KEY=eaed9e16-e999-4b16-a510-8a57eb740d97
+
+
+1st option - you can use config.PROPERTIES file for parameters
 
 OR
 
-2nd option - command line run using Maven with parameters.
+2nd option - command line run using MAVEN with parameters.
 export M2_HOME=/Users/krudrangi/apache-maven-3.6-2.3/
 export PATH=$PATH:/Users/krudrangi/apache-maven-3.6-2.3/bin
+
 # krudrangi@it-mbp-krudrangi (master *) ~/git/SelniumFrameWorkFull/AGSelFW: mvn install -DbrowserToBeTested=firefox -DurlToBeTested=https://www.qa.apartmentguide.com -DxmlFiles=testng.xml
+# krudrangi@it-mbp-krudrangi (master *) ~/git/SelniumFrameWorkFull/AGSelFW: mvn install -DbrowserToBeTested=sauce_ie -DurlToBeTested=https://www.qa.apartmentguide.com -DxmlFiles=testng.xml
--- AGSelFW/src/test/java/com/ag/utility/BrowserFactory.java
+++ AGSelFW/src/test/java/com/ag/utility/BrowserFactory.java
@@ -2,14 +2,22 @@ package com.ag.utility;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class BrowserFactory {
 	
 	public static WebDriver startLargeApp (WebDriver ldriver, String browserName, String launch_url) {
+		
+		final String USERNAME =  System.getenv("SAUCE_USERNAME");;
+		final String ACCESS_KEY = System.getenv("SAUCE_ACCESS_KEY");
+		final String SAUCE_URL = "https://"+USERNAME+":"+ACCESS_KEY+"@ondemand.us-west-1.saucelabs.com:443/wd/hub";
 		
 		if (browserName.equals("chrome") ) {
 			System.setProperty("webdriver.chrome.driver", ".//Drivers//chromedriver");
@@ -23,6 +31,34 @@ public class BrowserFactory {
 		else if (browserName.equals("safari")) {
 			System.setProperty("webdriver.safari.driver", "/usr/bin/safaridriver");
 			ldriver = new SafariDriver();
+		} 
+		else if (browserName.equals("sauce_chrome")) {
+			DesiredCapabilities caps =  DesiredCapabilities.chrome();
+			caps.setCapability("platform", "Windows 10");
+			caps.setCapability("version", "latest");
+			caps.setCapability("extendedDebugging", "true");
+			caps.setCapability("name", "Test Script Name");
+			caps.setCapability("buildNumber", "myBuild#3.0");
+			
+			try {
+				ldriver = new RemoteWebDriver(new java.net.URL(SAUCE_URL), caps);
+			} catch (Exception e) {
+				System.out.println("Unable to connect Saucelabs CHROME browser..." +e.getMessage());
+			}
+		} 
+		else if (browserName.equals("sauce_ie")) {
+			MutableCapabilities sauceOptions = new MutableCapabilities();
+			InternetExplorerOptions browserOptions = new InternetExplorerOptions();
+			browserOptions.setCapability("platformName", "Windows 10");
+			browserOptions.setCapability("browserVersion", "latest");
+			browserOptions.setCapability("sauce:options", sauceOptions);
+			browserOptions.setCapability("extendedDebugging", "true");
+			
+			try {
+				ldriver = new RemoteWebDriver(new java.net.URL(SAUCE_URL), browserOptions);
+			} catch (Exception e) {
+				System.out.println("Unable to connect Saucelabs IE browser..." +e.getMessage());
+			}
 		} 
 		else {
 			System.out.println("Sorry, We don't support this browser..");
